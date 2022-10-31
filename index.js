c5577179a6ca4850fb7cfe1f5ad8e74f9b5cc792
Mide001 <olamideisreal5@gmail.com>
2022-11-01 00:27:59 +0100

    updated

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name","req","Please provide your name"); 
-frmvalidator.addValidation("email","req","Please provide your email"); 
-frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+//get the form with ID
+
+if (typeof window !== 'undefined') {
+    //here `window` is available, so `window.document` (or simply `document`) is available too
+    const form = document.getElementById("contactForm");
+
+const eventForm = form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let mail = new FormData(form);
+
+    sendMail(mail);
+})
+
+const sendMail = (mail) => {
+    fetch("/send", {
+        method: "post",
+        body: mail,
+    }).then((response) => {
+        return response.json();
+    });
+};
+}
+
